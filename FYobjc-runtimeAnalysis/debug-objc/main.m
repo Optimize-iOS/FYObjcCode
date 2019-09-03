07ccbe220adb5d967545b133b51940b62b956fae
JackJin <1549793329@qq.com>
2019-09-03 16:57:31 +0800

    objc-runtime

---
+++ FYobjc-runtimeAnalysis/debug-objc/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by Closure on 2018/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+
+@interface FYObject : NSObject
+
+@end
+
+
+@implementation FYObject
+
++ (void)load {
+    NSLog(@"%@ class in %s", NSStringFromClass(self), __func__);
+}
+
+@end
